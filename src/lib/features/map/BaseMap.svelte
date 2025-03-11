c4ffd41d1b399ebd6438b548a6da9dacd56a26a6
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2025-03-11 02:00:38 +0200

    initialize project structure with Svelte, Tailwind CSS, and TypeScript setup

---
+++ src/lib/features/map/BaseMap.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { PUBLIC_MAP_KEY } from '$env/static/public';
+	import type { Map } from 'maplibre-gl';
+	import type { Snippet } from 'svelte';
+	import { MapLibre } from 'svelte-maplibre-gl';
+
+	type Props = {
+		children?: Snippet;
+	};
+	let { children } = $props() as Props;
+
+	let baseUrl = 'https://avoin-karttakuva.maanmittauslaitos.fi';
+	let style = `${baseUrl}/vectortiles/stylejson/v20/taustakartta.json?TileMatrixSet=WGS84_Pseudo-Mercator&api-key=${PUBLIC_MAP_KEY}`;
+
+	let map = $state<Map>();
+</script>
+
+<MapLibre
+	bind:map
+	class="h-full w-full"
+	{style}
+	zoom={4.59}
+	center={[26.544678803263537, 65.33426030226093]}
+>
+	{@render children?.()}
+</MapLibre>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import BaseMap from '$lib/features/map/BaseMap.svelte';
+</script>
+
+<main class="map-container">
+	<BaseMap></BaseMap>
+</main>
+
+<style>
+	.map-container {
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
